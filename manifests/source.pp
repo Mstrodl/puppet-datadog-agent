2fee1cf46d437aa7677d4fcbc86770ce2bfdd661
Mary Strodl <ipadlover8322@gmail.com>
2021-09-27 12:17:43 -0400

    oops, typo

--- manifests/source.pp
+++ manifests/source.pp
@@ -14,10 +14,10 @@ class datadog_agent::source(
   String $tags_quote_wrap = "\"${tags_join}\"",
 ) inherits datadog_agent::params {
   # Fake package that depends on our exec. Dumb solution
-  package { $datadog_agent::params::package_name,
+  package { $datadog_agent::params::package_name:
     command  => "/bin/echo",
     provider => "ports",
-    requires => Exec['installer']
+    requires => Exec['installer'],
   }
   exec { 'installer':
     command     =>
